ebd139165c4a47c2549cd294b5c45cc9e272a52b
Davla224 <David.Lagerqvist02@gmail.com>
2022-05-21 15:12:33 +0200

    New string

--- grid.py
+++ grid.py
@@ -18,8 +18,7 @@ class Tile(object):
 
     def __init__(self, static_obj, variable_obj = None) -> None:
         self.static_obj = static_obj
-        self.variable_obj = variable_obj
-        self.entities = None
+        #self.variable_obj = variable_obj
 
     def change_variable_obj(self, new_variable_obj):
         self.variable_obj = new_variable_obj
@@ -57,23 +56,23 @@ def print_array(array_inst:Array, x_range:int, y_range:int):
             row += array_inst.tile_inst_dict[(x_value, y_value)].static_obj + " "
         print(row)
 
-array_list = [[(1,1), "wall"], [(2,1), "wall"], [(3,1), "wall"], [(4,1), "wall"], [(1,2), "dirt"], [(2,2), "grass"]\
-    , [(3,2), "grass"], [(4,2), "wall"], [(1,3), "dirt"], [(2,3), "grass"], [(3,3), "water"], [(4,3), "wall"]\
-        , [(1,4), "dirt"], [(2,4), "grass"], [(3,4), "grass"], [(4,4), "wall"]]
-text = "wwww\ndggw\ndgvw\ndggw"
-symbol_dict = {"w":"wall", "g":"grass", "d":"dirt", "v":"water", "f":"floor", " ":"space"}
-text2 = "ddffd\nggffw\nggffw\nvv  w\nvvwww" 
-test_text1 = "wwwwwwfwww\n\
-    wffffffffw\n\
-    wfwwfffvfw\n\
-    wffffwwffw\n\
-    "
+if __name__ == "__main__":
+    array_list = [[(1,1), "wall"], [(2,1), "wall"], [(3,1), "wall"], [(4,1), "wall"], [(1,2), "dirt"], [(2,2), "grass"]\
+        , [(3,2), "grass"], [(4,2), "wall"], [(1,3), "dirt"], [(2,3), "grass"], [(3,3), "water"], [(4,3), "wall"]\
+            , [(1,4), "dirt"], [(2,4), "grass"], [(3,4), "grass"], [(4,4), "wall"]]
+    text = "wwww\ndggw\ndgvw\ndggw"
+    symbol_dict = {"w":"wall", "g":"grass", "d":"dirt", "v":"water", "f":"floor", " ":"space"}
+    text2 = "ddffd\nggffw\nggffw\nvv  w\nvvwww"
+
+    test_text1 = "wwwwwwfwww\nwffffffffw\nwfwwfffvfw\nwffffwwffw\nwffwfwfffw\nwffwfwfvfw\nwfvffffffw\nwfvfwwwffw\nwffffffffw\nwwwwwwwwww"
 
 
 
-array_info = convert_text_to_array_list(text, symbol_dict)
-array = Array(array_info)
-array_info2 = convert_text_to_array_list(text2, symbol_dict)
-array2 = Array(array_info2)
-print(array_info)
-print_array(array2, 5, 5)
+    array_info = convert_text_to_array_list(text, symbol_dict)
+    array = Array(array_info)
+    array_info2 = convert_text_to_array_list(text2, symbol_dict)
+    array2 = Array(array_info2)
+    array_info3 = convert_text_to_array_list(test_text1, symbol_dict)
+    array3 = Array(array_info3)
+    print(array_info3)
+    print_array(array3, 10, 10)
