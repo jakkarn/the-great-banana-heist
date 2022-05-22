d7515a78e8aab07694f64b8e77e267b634816bae
Davla224 <David.Lagerqvist02@gmail.com>
2022-05-22 11:56:55 +0200

    Update guard.py

--- guard.py
+++ guard.py
@@ -12,6 +12,7 @@ class Guard(Entity):
         if self.position == self.move_pattern[self.index_in_move_pattern]:
             index = (self.index_in_move_pattern + 1)%len(self.move_pattern)
             next_position = self.move_pattern[index]
+            self.index_in_move_pattern = index
         else:
             vector_to_old_pos = (self.move_pattern[self.index_in_move_pattern][0] - self.position[0]\
                 , self.move_pattern[self.index_in_move_pattern][1] - self.position[1])
@@ -27,16 +28,8 @@ class Guard(Entity):
                 print("Error in Guard.find_next_pos()")
         return next_position
 
-    def move(self, game_data):
-        grid = game_data.grid
+    def find_movement(self):
+        new_pos = self.find_next_poition()
+        x_vec = new_pos[0] - self.position[0]
+        y_vec = new_pos[1] - self.position[1]
 
-        new_position = self.find_next_pos()
-
-        self.last_direction = (new_position[0] - self.position[0], new_position[1] - self.position[1])
-        self.position = new_position
-
-        for banana_peel in game_data.get_banana_peels():
-            if banana_peel.position == self.position:
-                print("SLIP!")
-                self.is_slipping = True
-                game_data.remove_entity(banana_peel)
