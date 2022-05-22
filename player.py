dd8c08fb0449aa189c1d2cf2796a9614289d24df
jakkarn <jakno732@student.liu.se>
2022-05-22 16:00:16 +0200

    Revert accident

--- player.py
+++ player.py
@@ -1,17 +1,24 @@
 import pygame
 from banana_peel import BananaPeel
 from constants import IMAGE_BANANA, IMAGE_MONKEY, PLAYER_BLUE, TILE_SIZE, PLAYER_MAX_ENERGY, PLAYER_START_BANANA_COUNT
-from draw_utils import draw_circle, draw_font, draw_image
+from draw_utils import draw_circle, draw_death, draw_font, draw_image
 from entity import Entity
 from view_port import SCREEN
+from timer import Timer
 
 class Player(Entity):
     def __init__(self, position):
         super().__init__(position)
         self.energy = PLAYER_MAX_ENERGY
         self.banana_count = PLAYER_START_BANANA_COUNT
+        self.death_timer = Timer(0)
+        self.death_started = False
 
     def update(self, game_data):
+        if not self.alive:
+            self.death(game_data)
+            return
+
         self.check_if_winning(game_data)
         self.eat_banana_action(game_data)
         self.update_movement(game_data)
@@ -21,9 +28,14 @@ class Player(Entity):
             self.eat_banana(game_data)
 
     def draw(self):
+        draw_font(f"x {self.banana_count}", (14,1))
+
+        if not self.alive:
+            draw_death(self.position)
+            return
+
         draw_image(IMAGE_MONKEY, self.position)
         draw_image(IMAGE_BANANA, (13,1))
-        draw_font(f"x {self.banana_count}", (14,1))
 
     def update_movement(self, game_data):
         walked = False
@@ -40,6 +52,17 @@ class Player(Entity):
                     walked = self.walk((0, 1), game_data)
         if walked == True:
             self.energy -= 1
+
+    def death(self, game_data):
+        if not self.death_started:
+            self.death_started = True
+            self.death_timer = Timer(2)
+            return
+
+        if self.death_timer:
+            return
+
+        game_data.remove_entity(self)
 
     def pick_up_banana(self, game_data):
         bananas = game_data.get_bananas()
