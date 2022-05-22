644e69884942ea4a1eb4822084bb32805d470965
Oscar Vestlund <ove@lysator.liu.se>
2022-05-22 10:36:20 +0200

    Add banana eating based on energy

--- entity.py
+++ entity.py
@@ -14,9 +14,9 @@ class Entity:
 
     def walk(self, direction, game_data):
         if self.is_slipping:
-            return
+            return False
 
-        self.move(direction, game_data)
+        return self.move(direction, game_data)
 
     def move(self, movement, game_data):
         grid = game_data.grid
@@ -25,7 +25,7 @@ class Entity:
 
         if not grid.is_walkable(new_position):
             self.last_direction = (0,0)
-            return
+            return False
 
         self.last_direction = (new_position[0] - self.position[0], new_position[1] - self.position[1])
         self.position = new_position
@@ -34,3 +34,4 @@ class Entity:
             if banana_peel.position == self.position:
                 self.is_slipping = True
                 game_data.remove_entity(banana_peel)
+        return True
--- player.py
+++ player.py
@@ -13,36 +13,43 @@ class Player(Entity):
 
     def update(self, game_data):
         self.check_if_winning(game_data)
-        self.eat_banana(game_data)
+        self.eat_banana_action(game_data)
         self.update_movement(game_data)
+        if self.energy <= 0: 
+            self.eat_banana(game_data)
 
     def draw(self):
         draw_image(IMAGE_MONKEY, self.position)
 
     def update_movement(self, game_data):
+        walked = False
         super().update(game_data)
         for event in game_data.events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    self.walk((-1, 0), game_data)
+                    walked = self.walk((-1, 0), game_data)
                 if event.key == pygame.K_RIGHT:
-                    self.walk((1, 0), game_data)
+                    walked = self.walk((1, 0), game_data)
                 if event.key == pygame.K_UP:
-                    self.walk((0, -1), game_data)
+                    walked = self.walk((0, -1), game_data)
                 if event.key == pygame.K_DOWN:
-                    self.walk((0, 1), game_data)
+                    walked = self.walk((0, 1), game_data)
+        if walked == True:
+            self.energy -= 1
 
-    def eat_banana(self, game_data):
+    def eat_banana_action(self, game_data):
         for event in game_data.events:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                self.banana_count -= 1
-
-                game_data.entities += [BananaPeel(self.position)]
+                self.eat_banana(game_data)
 
     def check_if_winning(self, game_data):
         if game_data.grid.is_winningtile(self.position):
             print("you win!")
 
+    def eat_banana(self, game_data):
+        self.banana_count -= 1
+        self.energy = PLAYER_MAX_ENERGY
+        game_data.entities += [BananaPeel(self.position)]
     #def banana_action_pressed():
     #    for event in pygame.event.get():
     #        if event.key == K_SPACE:
